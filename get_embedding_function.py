bf0e58e42a95c0ab435b7bba4cd49afd9ed7eda3
Balaji <balaji.ajai0407@gmail.com>
2025-12-04 12:03:42 +0530

    Add OLLAMA_BASE_URL support for remote Ollama

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,4 +1,5 @@
 from langchain_ollama import OllamaEmbeddings
+import os
 # from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
 
@@ -6,5 +7,7 @@ def get_embedding_function():
     # embeddings = BedrockEmbeddings(
     #     credentials_profile_name="default", region_name="us-east-1"
     # )
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    # Get Ollama base URL from environment, default to localhost
+    ollama_base_url = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
+    embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=ollama_base_url)
     return embeddings
